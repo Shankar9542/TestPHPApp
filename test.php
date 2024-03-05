8123de746fec217a096796b7eb3d8fa2d0758737
Shankar9542 <148524491+Shankar9542@users.noreply.github.com>
2024-03-05 17:00:00 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,5 @@
+<?php
+echo "This is an exaple of AWS CI CD Pipeline using Github/AWS Code pipeline/AWS ElasticBeanstalk";
+//echo "<br>";
+//echo "We" can see changes reflecting real time!! Great!";
+?>
